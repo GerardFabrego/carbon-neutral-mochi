85c5d668b3ca20e5af5763ce05e4f0e55e3eca32
gerardfabregotw <gerard.fabrego@thoughtworks.com>
2022-09-22 17:35:50 +0200

    Add ray casting and deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { useEffect } from 'react';
 
 import * as THREE from 'three';
-// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-// import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 import SceneInit from './lib/SceneInit';
@@ -14,32 +11,38 @@ function App() {
     test.initialize();
     test.animate();
 
-    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
-    // const boxMaterial = new THREE.MeshNormalMaterial();
-    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    // test.scene.add(boxMesh);
-
-    let loadedModel;
+    // add shiba model
     const glftLoader = new GLTFLoader();
     glftLoader.load('./shiba/scene.gltf', (gltfScene) => {
-      loadedModel = gltfScene;
-      // console.log(loadedModel);
-
-      gltfScene.scene.rotation.y = Math.PI / 8;
-      gltfScene.scene.position.y = 3;
+      // gltfScene.scene.position.y = 0.5;
+      // gltfScene.scene.position.z = 4;
       gltfScene.scene.scale.set(10, 10, 10);
       test.scene.add(gltfScene.scene);
     });
 
-    const animate = () => {
-      // if (loadedModel) {
-      //   loadedModel.scene.rotation.x += 0.01;
-      //   loadedModel.scene.rotation.y += 0.01;
-      //   loadedModel.scene.rotation.z += 0.01;
-      // }
-      requestAnimationFrame(animate);
+    const origin = new THREE.Vector3();
+    origin.y = 15;
+    const direction = new THREE.Vector3();
+    direction.y = -1;
+    const raycaster = new THREE.Raycaster();
+
+    raycaster.set(origin, direction);
+    raycaster.intersectObjects(test.scene.children)
+
+    const onClick = () => {
+
+      raycaster.set(origin, direction);
+      const intersects = raycaster.intersectObjects(test.scene.children)
+
+      console.log(intersects);
+
+      // change color of the closest object intersecting the raycaster
+      if (intersects.length > 0) {
+        intersects[0].object.material.color.set(0xff0000);
+      }
     };
-    animate();
+
+    window.addEventListener('click', onClick);
   }, []);
 
   return (
